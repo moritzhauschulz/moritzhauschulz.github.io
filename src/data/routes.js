93c02e3e11898e62a1d5393cee42720ad8d61cf3
Moritz Hauschulz <moritz.hauschulz@gmail.com>
2024-10-03 18:53:42 +0100

    bla

--- src/data/routes.js
+++ src/data/routes.js
@@ -4,22 +4,22 @@ const routes = [
     label: "Michael D'Angelo",
     path: '/',
   },
-  // {
-  //   label: 'About',
-  //   path: '/about',
-  // },
-  // {
-  //   label: 'Resume',
-  //   path: '/resume',
-  // },
-  // {
-  //   label: 'Projects',
-  //   path: '/projects',
-  // },
-  // {
-  //   label: 'Stats',
-  //   path: '/stats',
-  // },
+  {
+    label: 'About',
+    path: '/about',
+  },
+  {
+    label: 'Resume',
+    path: '/resume',
+  },
+  {
+    label: 'Projects',
+    path: '/projects',
+  },
+  {
+    label: 'Stats',
+    path: '/stats',
+  },
   {
     label: 'Contact',
     path: '/contact',
