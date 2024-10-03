ac43cb2a21eb3d05290c7f1dc618b29b2976a098
Moritz Hauschulz <moritz.hauschulz@gmail.com>
2024-10-03 17:12:28 +0100

    img

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,9 +12,9 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
+        <h2>Mortiz E. Hauschulz</h2>
         <p>
-          <a href="mailto:michael@mldangelo.com">michael@mldangelo.com</a>
+          <a href="mailto:moritz.hauschulz@stx.ox.ac.uk">moritz.hauschulz@stx.ox.ac.uk</a>
         </p>
       </header>
     </section>
@@ -22,13 +22,14 @@ const SideBar = () => (
     <section className="blurb">
       <h2>About</h2>
       <p>
-        Hi, I&apos;m Michael. I am a{' '}
-        <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC
-        alumnus, and the co-founder and CTO of{' '}
-        <a href="https://promptfoo.dev">Promptfoo</a>. Previously, I
-        was VP of Engineering at <a href="https://usesmileid.com">SmileID</a>,
-        co-founder and CTO of <a href="https://arthena.com">Arthena</a>, and
-        co-founded <a href="https://matroid.com">Matroid</a>.
+        I&apos;m Moritz. I am interested in the mathematical fundamentals of disruptive technologies. I recently started to study towards the MSc in Mathematical Sciences 
+        at the <a href="https://www.ox.ac.uk/admissions/undergraduate/courses/course-listing/mathematics">University of Oxford</a>, and am actively searching doctoral positions in the space of 
+        machine learning foundations with applications in scientific research including weather, fusion and quantum.
+        My recent work focuses on generative modelling in discrete spaces with energy based models, 
+        and I wrote a {' '}<a href="https://github.com/moritzhauschulz/samplingEBMs"> thesis </a>
+        on this topic at <a href="https://www.imperial.ac.uk/computing/">Imperial</a>.
+        I was an undergraduate at <a href="https://www.lse.ac.uk/economics">LSE</a>, where I met the amazing people whom I helped build  <a href="https://alexandria-media.org/">Alexandria</a> before starting my graduate studies.
+        My work has been generously supported by <a href="https://www.sdw.org/index.html">sdw</a> for many years, and I am currently president of their UK group.
       </p>
       <ul className="actions">
         <li>
