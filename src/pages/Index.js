a3505b06873925421e12c94ad2ea1286158d4196
Moritz Hauschulz <moritz.hauschulz@gmail.com>
2024-10-16 23:33:58 +0100

    fixed error in index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -23,7 +23,7 @@ const Index = () => (
       </header>
       <p>
         {' '}
-        I am student in mathematics with an interdisciplinary background. To know more, check out my
+        I am student in mathematics with an interdisciplinary background. To know more, check out my {' '}
         <Link to="/resume">resume</Link> and selected <Link to="/projects">projects</Link>. For collaborations, opportunities or if you just want to have a chat, feel free to {' '}
         <Link to="/contact">contact</Link> me.
       </p>
