87f6392dcac4e610cf6a2b15f33011d41411867a
Moritz Hauschulz <moritz.hauschulz@gmail.com>
2024-12-12 15:28:04 +0000

    updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,15 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Discrete Flow Sampling with Trajectory Balance (Research Proposal)',
+    subtitle: 'RESEARCH PROPOSAL',
+    image: '/images/projects/TB_thumbnail.png',
+    link: 'https://github.com/moritzhauschulz/samplingEBMs/blob/f7cd526c22000ceecf592198710b965b10478e4e/Discrete_Flow_Sampling_with_Trajectory_Balance___Ideas.pdf',
+    date: '2024-10-01',
+    desc:
+      'I present some ideas which I intend to explore in my PhD. These are based on insights from my research project on "Training Energy-Based Models by Learning to Sample with Discrete Flows."',
+  },
+  {
     title: 'Training Energy-Based Models by Learning to Sample with Discrete Flows',
     subtitle: 'MSc Thesis I',
     image: '/images/projects/heatmap_thumbnails.png',
@@ -10,13 +19,31 @@ const data = [
       'In this research project I used discrete flows to amortize the generation of negative samples for training energy-based models. My work was supervised by Yingzhen Li and Zijing Ou at Imperial.',
   },
   {
+    title: 'Project on Graph-based Super-Resolution of Brain Connectivity Matrices',
+    subtitle: 'Coursework at Imperial College',
+    image: '/images/projects/GraphNetbyNet.png',
+    link: 'https://raw.githubusercontent.com/moritzhauschulz/moritzhauschulz.github.io/main/public/docs/Deep_Graph_Learning_Report_Anon.pdf',
+    date: '2024-03-01',
+    desc:
+      'In this group project we developed a new framework for brain graph super-resolution.',
+  },
+  {
+    title: 'Project on Detecting Patronizing Language',
+    subtitle: 'Coursework at Imperial College',
+    image: '/images/projects/NLP_thumbnail.png',
+    link: 'https://raw.githubusercontent.com/moritzhauschulz/moritzhauschulz.github.io/main/public/docs/NLP_Courswork_website.pdf',
+    date: '2024-03-01',
+    desc:
+      'In this project I fine-tuned a RoBERTa LLM for the task of detecting partonizing and condescending language.',
+  },
+  {
     title: 'Sovereign Commitment in ECB Announcements',
     subtitle: 'BSC Thesis',
     image: '/images/projects/EME_thumbnail.png',
     link: 'https://raw.githubusercontent.com/moritzhauschulz/moritzhauschulz.github.io/main/public/docs/EME_Dissertation.pdf',
     date: '2021-08-01',
     desc:
-      "In this research project I conducted a factor analysis of the market reaction to ECB announcements, and was able to identify a novel causal channel that I label 'Sovereign Commitment'. This work was supervised by Shengxing Zhang at LSE.",
+      'In this research project I conducted a factor analysis of the market reaction to ECB announcements, and was able to identify a novel causal channel that I label "Sovereign Commitment". This work was supervised by Shengxing Zhang at LSE.',
   },
 ];
 
